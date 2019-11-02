7885817c507cbb1b8c1de4d3a84689e1d84f0271
rzeczy <sprzedazrzeczy@onet.pl>
2019-11-02 23:16:28 +0000

    Apply fixes from StyleCI

--- src/Foo.php
+++ src/Foo.php
@@ -1,12 +1,20 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace Foo;
-class Foo {
-  private $value;
-  public function __construct($value){
-    $this->value=$value;
+
+class Foo
+{
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
     }
-    
-    public function value(){
-    return $this->value;
+
+    public function value()
+    {
+        return $this->value;
     }
 }
